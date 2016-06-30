53653b292b317f6907688515a45888e0aad9c2ed
Zarandy <zarandy.peter@gmail.com>
2016-07-01 01:38:51 +0200

    matrix inverse??

--- src/algebra/Matrix.java
+++ src/algebra/Matrix.java
@@ -52,6 +52,17 @@ public class Matrix {
 	}
 	
 	
+	public static Matrix unit(int n) {
+		Matrix m = new Matrix(n,n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				m.m_[i][j] = (i==j) ? Rational.ONE : Rational.ZERO;
+			}			
+		}
+		return m;
+	}
+	
+	
 	public Matrix elem_basis_transformation(int row, int col) {
 		Matrix m = new Matrix(row_num_,column_num_);
 		for (int j = 0; j < column_num_; j++) {
@@ -85,9 +96,92 @@ public class Matrix {
 					lin_independents.add(j);
 					break;
 				}
-			}			
+			}
 		}
 		return lin_independents.size();
+	}
+	
+	
+	
+	private Matrix inverse_pivot(int row, int col) {
+		Matrix m = new Matrix(row_num_,column_num_);
+		for (int j = 0; j < column_num_; j++) {
+			if (j == col) {
+				m.m_[row][j] = m_[row][col].inverse();
+				continue;
+			}
+			m.m_[row][j] = m_[row][j].divide(m_[row][col]); 
+		}
+		for (int i = 0; i < row_num_; i++) {
+			if (i == row) continue;
+			for (int j = 0; j < column_num_; j++) {
+				if (j == col) {
+					m.m_[i][j] = m.m_[row][col].times(m_[i][col]).opposite();
+					continue;
+				}
+				m.m_[i][j] = m_[i][j].minus(m.m_[row][j].times(m_[i][col]));
+			}
+		}
+		return m;		
+	}
+	
+	
+	
+	public Matrix inverse() {
+		lin_independents = new ArrayList<Integer>();
+		Matrix matrix = new Matrix(this);
+		for (int i = 0; i < row_num_; i++) {
+			for (int j = 0; j < column_num_; j++) {
+				if (!matrix.m_[i][j].equals(Rational.ZERO)) {
+					matrix = matrix.inverse_pivot(i, j);
+					lin_independents.add(j);
+					break;
+				}
+				if (j+1==column_num_) 
+					throw new ArrayIndexOutOfBoundsException("not invertible");
+			}
+		}
+		return row_permutation(lin_independents).times(matrix).
+				times(column_permutation(lin_independents));		
+	}
+
+
+
+	public Matrix times(Matrix matrix) {
+		Matrix m = new Matrix(row_num_,matrix.column_num_);
+		for (int i = 0; i < m.row_num_; i++) {
+			for (int j = 0; j < m.column_num_; j++) {
+				m.m_[i][j] = Rational.ZERO;
+				for (int k = 0; k < column_num_; k++)
+					m.m_[i][j] = m.m_[i][j].plus(m_[i][k].times(matrix.m_[k][j]));
+			}
+		}
+		return m;
+	}
+
+
+
+	public static Matrix row_permutation(ArrayList<Integer> columns) {
+		int n = columns.size();
+		Matrix m = new Matrix(n,n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				m.m_[i][j] = (columns.get(i)==j) ? Rational.ONE : Rational.ZERO;
+			}			
+		}
+		return m;
+	}
+
+
+	public static Matrix column_permutation(ArrayList<Integer> columns) {
+		int n = columns.size();
+		Matrix m = new Matrix(n,n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				m.m_[i][j] = (columns.get(j)==i) ? Rational.ONE : Rational.ZERO;
+			}			
+		}
+		return m;
 	}
 	
 	
--- src/test/TestMatrix.java
+++ src/test/TestMatrix.java
@@ -10,10 +10,13 @@ public class TestMatrix {
 	
 	public static void main(String[] args) {
 		System.out.println("TestMatrix");
-		test_matrix.m_[0][0] = Rational.ONE;
+		test_matrix.m_[0][0] = Rational.valueOf(1, 1);
 		test_matrix.m_[0][1] = Rational.valueOf(3, 1);
 		test_matrix.m_[1][0] = Rational.valueOf(2, 1);
 		test_matrix.m_[1][1] = Rational.valueOf(4, 1);
+		Matrix inverse = test_matrix.inverse();//?????
+		System.out.println(inverse.m_[0][0]+","+inverse.m_[0][1]);
+		System.out.println(inverse.m_[1][0]+","+inverse.m_[1][1]);
 		Matrix new_m = test_matrix.elem_basis_transformation(0, 0);
 		assert new_m.m_[0][1].equals(Rational.valueOf(3,1));	
 		assert new_m.m_[1][1].equals(Rational.valueOf(-2,1));
